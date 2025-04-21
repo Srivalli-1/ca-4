a6911a08a214eb876de414511739456f4b1f496a
Srivalli-1 <kajasrivalli5@gmail.com>
2025-04-21 18:54:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose')
+
+const express=require('express')
+const cors=require('cors')
+const app=express()
+app.use(express.json())
+
+app.use(cors())
+
+const dotenv=require('dotenv')
+dotenv.config()
+
+const PORT=process.env.PORT||4004
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log('mongodb connected'))
+.catch((err)=> console.log(err))
+
+const abc=require('./Routes')
+app.use('/routes',abc)
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running susscessfully ${PORT}`)
+})
